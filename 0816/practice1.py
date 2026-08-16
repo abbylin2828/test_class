4a1a068f4d48976589399cb6af2d0f04b504705c
abbylin2828 <rainbow5930@gmail.com>
2026-08-16 13:43:43 +0800

    0816 practice2 upload

---
+++ 0816/practice1.py
@@ -0,0 +1,12 @@
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client()
+
+interaction = client.interactions.create(
+    model="gemini-3.5-flash",
+    input="天空為什麼是藍的"
+)
+print(interaction.output_text)
